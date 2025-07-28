622a63b06775f8498894f46684bbc9b82cb8a236
Rana Darpan <ranadarpan0@gmail.com>
2025-07-28 17:25:28 +0530

    Updated Group API

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -1,53 +1,40 @@
-// import express from "express";
-// import {
-//   createGroup,
-//   getGroupById,
-//   updateGroup,
-//   deleteGroup,
-//   getUserGroups,  
-//   getGroupMembers,
-//   addMemberToGroup,
-//   removeMemberFromGroup,
-//   getGroupByCode,
-//   joinGroupByCode,
-//   searchPublicGroups,
-//   checkIsMember,
-// } from "../controllers/groupController.js";
-// import { authMiddleware } from "../middleware/authMiddleware.js";
-
-// const router = express.Router();
-
-// // Public Routes
-// router.get("/groups/code/:code", getGroupByCode);
-// router.post("/groups/search", searchPublicGroups);
-
-// // Protected Routes (require authentication)
-// router.post("/groups", authMiddleware, createGroup);
-
-// router.get("/groups/:id", authMiddleware, getGroupById);
-// router.put("/groups/:id", authMiddleware, updateGroup);
-// router.delete("/groups/:id", authMiddleware, deleteGroup);
-// router.get("/groups", authMiddleware, getUserGroups);
-// router.get("/groups/:id/members", authMiddleware, getGroupMembers);
-// router.post("/groups/:id/members", authMiddleware, addMemberToGroup);
-// router.delete(
-//   "/groups/:id/members/:userId",
-//   authMiddleware,
-//   removeMemberFromGroup
-// );
-// router.post("/groups/:id/join", authMiddleware, joinGroupByCode);
-// router.get("/groups/:id/is-member", authMiddleware, checkIsMember);
-
-// export default router;
-
-
+// routes/groupRoutes.js
 import express from "express";
-import { createGroup } from "../controllers/groupController.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
+import {
+  createGroup,
+  getGroupById,
+  updateGroup,
+  deleteGroup,
+  getUserGroups,  
+  getGroupMembers,
+  addMemberToGroup,
+  removeMemberFromGroup, // This is the correct function name
+  getGroupByCode,
+  joinGroupByCode,
+  searchPublicGroups,
+  checkIsMember,
+} from "../controllers/groupController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// Public Routes
+router.get("/code/:code", getGroupByCode);
+router.get("/search", searchPublicGroups);
+
 // Protected Routes (require authentication)
-router.post("/groups", authMiddleware, createGroup);
+router.post("/", protect, createGroup);
+router.get("/:id", protect, getGroupById);
+router.put("/:id", protect, updateGroup);
+router.delete("/:id", protect, deleteGroup);
+router.get("/", protect, getUserGroups);
+router.get("/:id/members", protect, getGroupMembers);
+router.post("/:id/members", protect, addMemberToGroup);
+
+
+router.delete("/:id/members/:userId", protect, removeMemberFromGroup);
+
+router.post("/:id/join", protect, joinGroupByCode);
+router.get("/:id/is-member", protect, checkIsMember);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js'; // Assuming you have a DB connection file
 import userRoutes from './routes/userRoutes.js';
 import timeTableRoutes from "./routes/timetableRoutes.js"
+// import groups from "./controllers/groupController.js";
+import groups from "./routes/groupRoutes.js"
 import bcrypt from "bcrypt"
 // ... other imports
 
@@ -18,7 +20,7 @@ app.use(express.json());
 // Routes
 app.use('/api/users', userRoutes);
 app.use("/api/timetable",timeTableRoutes);
-
+app.use("/api/groups",groups);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
